5d24ca49f7b07cc788473e277350cfce073a017f
mexicainn225 <mexicainchoco0319@gmail.com>
2026-06-21 16:40:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ if __name__ == '__main__':
     Thread(target=run_web, daemon=True).start()
     if TOKEN:
         bot_app = ApplicationBuilder().token(TOKEN).build()
-        bot_app.add_handler(CommandHandler("start", lambda u, c: u.message.reply_text("🚀 Bienvenue ! Accédez aux signaux.")))
+        bot_app.add_handler(CommandHandler("start", lambda u, c: u.message.reply_text("🚀 Bienvenue !")))
         bot_app.run_polling()
